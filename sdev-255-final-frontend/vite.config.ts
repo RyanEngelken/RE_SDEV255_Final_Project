3a8e65f65a18f3adafd02625f9af1ca7bfc2e14e
RyanEngelken <75546339+RyanEngelken@users.noreply.github.com>
2025-04-25 16:26:16 -0400

    deploy to github pages

--- sdev-255-final-frontend/vite.config.ts
+++ sdev-255-final-frontend/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RE_SDEV255_Final_Project/'
+  base: '/RE_SDEV255_Final_Project/sdev-255-final-frontend/'
 });
