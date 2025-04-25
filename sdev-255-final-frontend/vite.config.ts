b88d04c6875d3d26b58a81444f482484740a954a
RyanEngelken <75546339+RyanEngelken@users.noreply.github.com>
2025-04-25 16:00:36 -0400

    build files for github pages

--- sdev-255-final-frontend/vite.config.ts
+++ sdev-255-final-frontend/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/RE_SDEV255_Final_Project/sdev-255-final-frontend/'
 })
